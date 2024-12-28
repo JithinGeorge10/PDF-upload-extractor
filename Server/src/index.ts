a7f85f6d0fca57b70ff4c3f52ff0d401b87b9113
JithinGeorge10 <jithin.george.el@gmail.com>
2024-12-28 10:37:14 +0530

    cors bug fix

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 
 app.use(cors({
-    origin: process.env.FRONTEND || '*' ||  'http://localhost:5173',
+    origin: process.env.FRONTEND || '*',
     methods:['GET','POST'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true 
